7f6179c841dd80abd39866f79a1fdad747ee9742
Prakritish Ghosh <102139041+PGMAC29@users.noreply.github.com>
2024-07-01 21:48:09 +0530

    Add files via upload
    chapter 3

--- port-folio website/index.js
+++ port-folio website/index.js
@@ -5,4 +5,9 @@ $(".menu").click(function(){
     $(".drop>ul").css({"background-color":"#9F6E49","padding":"10px","margin":"5px"});
 
   });
-  
+
+  $(".intro").click(function(){
+    $(".drop-down").css({
+        "display":"none"
+    });
+  });
